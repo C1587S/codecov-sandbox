f2c5cfcd0295bc7600602b50a9764d77a21004e2
Sebastian Cadavid Sanchez <s.cadavid1587@gmail.com>
2023-12-13 02:57:52 -0500

    Add example contents

---
+++ src/main.py
@@ -0,0 +1,11 @@
+def add_numbers(a, b):
+    """_summary_
+
+    Args:
+        a (float): Real number
+        b (float): Real number
+
+    Returns:
+        float: Real number (addition of a and b)
+    """
+    return a + b
