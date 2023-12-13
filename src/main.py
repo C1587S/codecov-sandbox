1f3490af52767219fa5ca0b2ef83998d4f185b4e
Sebastian Cadavid Sanchez <s.cadavid1587@gmail.com>
2023-12-13 03:10:56 -0500

    Add test

--- src/main.py
+++ src/main.py
@@ -9,3 +9,15 @@ def add_numbers(a, b):
         float: Real number (addition of a and b)
     """
     return a + b
+
+def multiply_numbers(a, b):
+    """_summary_
+
+    Args:
+        a (float): Real number
+        b (float): Real number
+
+    Returns:
+        float: Real number (multiplication of a and b)
+    """
+    return a * b
