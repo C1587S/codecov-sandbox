301c8b6d608a62ff5ed7b15a482a6a933dd584fd
Sebastian Cadavid Sanchez <s.cadavid1587@gmail.com>
2023-12-13 03:57:16 -0500

    Add unused function

---
+++ src/unused.py
@@ -0,0 +1,11 @@
+def divide_numbers(a, b):
+    """_summary_
+
+    Args:
+        a (float): Real number
+        b (float): Real number
+
+    Returns:
+        float: Real number (division between a and b)
+    """
+    return a / b
